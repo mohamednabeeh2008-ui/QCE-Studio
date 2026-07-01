7342a1cdb5ce553c8f70229a72a18174f9205aa7
mohamednabeeh2008-ui <mohamednabeeh2008@gmail.com>
2026-07-01 09:43:40 +0300

    refactor(core): export public api

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,8 +3,27 @@
  * Core Package Entry Point
  */
 
-export const QCE_VERSION = "0.1.0";
+export * from "./constants";
+export * from "./types";
+export * from "./utils";
 
+export * from "./enums/knowledge-status.enum";
+
+export * from "./errors/validation.error";
+
+export * from "./models/base.model";
+export * from "./models/knowledge-object.model";
+export * from "./models/template.model";
+export * from "./models/ontology-entity.model";
+
+export * from "./repositories/knowledge.repository";
+export * from "./repositories/memory-knowledge.repository";
+
+export * from "./services/knowledge.service";
+
+export * from "./validators/knowledge.validator";
+
+export const QCE_VERSION = "0.1.0";
 export const QCE_NAME = "QCE Studio";
 
 export function getVersion(): string {
