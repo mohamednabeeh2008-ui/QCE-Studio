057e5a716d065a7e78cc98c276512aa617d04133
mohamednabeeh2008-ui <mohamednabeeh2008@gmail.com>
2026-07-01 09:23:40 +0300

    feat(core): initialize core package entry

---
+++ packages/core/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * QCE Studio
+ * Core Package Entry Point
+ */
+
+export const QCE_VERSION = "0.1.0";
+
+export const QCE_NAME = "QCE Studio";
+
+export function getVersion(): string {
+  return QCE_VERSION;
+}
