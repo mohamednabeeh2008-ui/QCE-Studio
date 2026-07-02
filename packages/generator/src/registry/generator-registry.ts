04ee939a2e21c801619838d167d456c1cf032387
mohamednabeeh2008-ui <mohamednabeeh2008@gmail.com>
2026-07-02 06:55:28 +0300

    feat(generator): register aggregate generator

--- packages/generator/src/registry/generator-registry.ts
+++ packages/generator/src/registry/generator-registry.ts
@@ -10,4 +10,14 @@ export const generatorRegistry: GeneratorDefinition[] = [
     suffix: ".value-object.ts",
     template: "value-object",
   },
+  {
+    type: "entity",
+    suffix: ".entity.ts",
+    template: "entity",
+  },
+  {
+    type: "aggregate",
+    suffix: ".aggregate.ts",
+    template: "aggregate",
+  },
 ];
